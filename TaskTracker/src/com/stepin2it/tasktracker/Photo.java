faef61d3c69980d729140fea436c07a9e81a20cb
Stepin2 IT <stepin2itinc@gmail.com>
2014-01-19 15:28:16 -0500

    Merging with dev

---
+++ TaskTracker/src/com/stepin2it/tasktracker/Photo.java
@@ -0,0 +1,43 @@
+package com.stepin2it.tasktracker;
+
+public class Photo
+{
+	private int photoId;
+	private String photoPath;
+	private String photoTitle;
+	private String photoDescription;
+	public int getPhotoId()
+	{
+		return photoId;
+	}
+	public String getPhotoPath()
+	{
+		return photoPath;
+	}
+	public String getPhotoTitle()
+	{
+		return photoTitle;
+	}
+	public String getPhotoDescription()
+	{
+		return photoDescription;
+	}
+	public void setPhotoId(int photoId)
+	{
+		this.photoId = photoId;
+	}
+	public void setPhotoPath(String photoPath)
+	{
+		this.photoPath = photoPath;
+	}
+	public void setPhotoTitle(String photoTitle)
+	{
+		this.photoTitle = photoTitle;
+	}
+	public void setPhotoDescription(String photoDescription)
+	{
+		this.photoDescription = photoDescription;
+	}
+	
+
+}
