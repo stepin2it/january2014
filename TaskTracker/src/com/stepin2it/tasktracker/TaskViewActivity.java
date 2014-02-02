edfc3416d935606a6d4da721fb3399eaf67ba712
Stepin2 IT <stepin2itinc@gmail.com>
2014-02-02 12:27:24 -0500

    Added xml layout for TaskViewActivity:

--- TaskTracker/src/com/stepin2it/tasktracker/TaskViewActivity.java
+++ TaskTracker/src/com/stepin2it/tasktracker/TaskViewActivity.java
@@ -1,6 +1,5 @@
 package com.stepin2it.tasktracker;
 
-import android.R;
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -25,34 +24,9 @@ public class TaskViewActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		Log.d(TAG, "TaskViewActivity onCreate");
-		// Creating programmatically instead of xml
-		// Please debug the following code so that 
-		// the fields for the task display correctly
-		LinearLayout layout = new LinearLayout(this);
-		layout.setOrientation(LinearLayout.VERTICAL);
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
-		layout.setLayoutParams(params);
+		setContentView(R.layout.activity_taskview);
 		
-		TextView idTextView = new TextView(this);
-		TextView titleTextView = new TextView(this);
-		
-		layout.addView(idTextView);
-		layout.addView(titleTextView);
-		
-		long taskId = this.getIntent().getLongExtra("TASK_ID", 0);
-		
-		idTextView.setText(String.valueOf(taskId));
-
-		final DatabaseAdapter db = new DatabaseAdapter(getApplicationContext());
-		db.open();
-
-		Cursor c = db.getRecord(taskId);
-		
-		if (c.moveToFirst())
-		{
-			titleTextView.setText(c.getString(1));
-		}
-	
+			
 	}
 
 }
